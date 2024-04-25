f62aeaae9ac69558e7ba3f6a7abba81c33999f03
Simeon Tran <siatran@ucsc.edu>
2024-04-25 12:40:23 -0700

    I think I can get the current timestamp.

--- main/time.c
+++ main/time.c
@@ -1,12 +1,18 @@
-#include "time.h"
-
 /**
- * The code to get the current time comes from the ESP-IDF documentatation:
+ * All of the code in the documention is both taken from and based upon the code
+ * from the "System Time" page from the ESP-IDF documentation:
  * https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html
 */
-int64_t getCurrentTime() {
-struct timeval tv_now;
-gettimeofday(&tv_now, NULL);
-int64_t time_us = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
-return time_us;
+
+#include "time.h"
+
+inline int64_t timevalToMicro(struct timeval time) {
+  return (int64_t)time.tv_sec * 1000000L + (int64_t)time.tv_usec;
+}
+
+int64_t getCurrentTime()
+{
+  struct timeval tvNow;
+  gettimeofday(&tvNow, NULL);
+  return timevalToMicro(tvNow);
 }
