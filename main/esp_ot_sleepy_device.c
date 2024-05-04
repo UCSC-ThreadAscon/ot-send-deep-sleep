2e238701c887757b67c001b6bf278ac62e1071d4
Simeon Tran <siatran@ucsc.edu>
2024-05-03 22:42:57 -0700

    Saving what I have so far

--- main/esp_ot_sleepy_device.c
+++ main/esp_ot_sleepy_device.c
@@ -135,28 +135,24 @@ int compare (const void* ptr1, const void* ptr2) {
 
 void app_main(void)
 {
-    // Used eventfds:
-    // * netif
-    // * ot task queue
-    // * radio driver
-    esp_vfs_eventfd_config_t eventfd_config = {
-        .max_fds = 3,
-    };
+  // Used eventfds:
+  // * netif
+  // * ot task queue
+  // * radio driver
+  esp_vfs_eventfd_config_t eventfd_config = {
+      .max_fds = 3,
+  };
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_vfs_eventfd_register(&eventfd_config));
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_vfs_eventfd_register(&eventfd_config));
 
-    xTaskCreate(ot_task_worker, "ot_power_save_main", 4096, NULL, 5, NULL);
+  xTaskCreate(ot_task_worker, "ot_power_save_main", 4096, NULL, 5, NULL);
 
-    struct timeval tvNow = getCurrentTimeval(); 
-    struct timeval tvExp = getFutureTimeval(EXP_TIME_SECONDS);
+  struct timeval tvNow = getCurrentTimeval(); 
+  struct timeval tvExp = getFutureTimeval(EXP_TIME_SECONDS);
 
-    /**
-     * TO-DO:
-     *  - Remove duplicates
-    */
   int64_t eventsUs[NUM_EVENTS_TEST];
   EmptyMemory(eventsUs, sizeof(eventsUs));
 
