853787efab6aabb2024f738bfd30a2c609319a5a
Simeon Tran <siatran@ucsc.edu>
2024-05-05 15:41:53 -0700

    Added hexdump API

---
+++ components/utilities/hexdump.c
@@ -0,0 +1,29 @@
+#include "hexdump.h"
+
+void printHexDump(otLogHexDumpInfo *aInfo) {
+  otError error = OT_ERROR_NONE;
+
+  while (error == OT_ERROR_NONE) {
+    error = otLogGenerateNextHexDumpLine(aInfo);
+
+    if (error == OT_ERROR_NONE) {
+      otLogNotePlat("%s", aInfo->mLine);
+    }
+    else if (error != OT_ERROR_NOT_FOUND) {
+      otLogCritPlat(otThreadErrorToString(error));
+    }
+  }
+
+  return;
+}
+
+void hexDump(void *data, uint16_t dataLength, const char* title) {
+  otLogHexDumpInfo hexDump;
+
+  hexDump.mDataBytes = (uint8_t *) data;
+  hexDump.mDataLength = dataLength;
+  hexDump.mTitle = title;
+
+  printHexDump(&hexDump);
+  return;
+}
---
+++ components/utilities/include/hexdump.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <openthread/logging.h>
+
+void hexDump(void *data, uint16_t dataLength, const char* title);
