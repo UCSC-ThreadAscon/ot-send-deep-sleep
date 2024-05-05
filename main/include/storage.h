9da3990ee359fb00e013805dd18eca2e11fadfda
Simeon Tran <siatran@ucsc.edu>
2024-05-05 13:23:18 -0700

    Starting to work on the nvs api

--- main/include/storage.h
+++ main/include/storage.h
@@ -1,1 +1,17 @@
+/**
+ * My Storage API is based upon the NVS Binary Large Object example code
+ * in the ESP-IDF Github repository:
+ * https://github.com/espressif/esp-idf/tree/v5.2.1/examples/storage/nvs_rw_blob
+*/
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
+
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+
+#define NVS_EVENTS_ARRAY "events"
---
+++ main/storage/storage.c
@@ -0,0 +1,6 @@
+/**
+ * My Storage API is based upon the NVS Binary Large Object example code
+ * in the ESP-IDF Github repository:
+ * https://github.com/espressif/esp-idf/tree/v5.2.1/examples/storage/nvs_rw_blob
+*/
+#include "storage.h"
