20a0cb530fce96d51b80c9268a80b57abdb8d35c
Simeon Tran <siatran@ucsc.edu>
2024-05-05 14:59:46 -0700

    UUID macros

--- main/include/uuid.h
+++ main/include/uuid.h
@@ -4,5 +4,17 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+/**
+ * Bytes containing the UUID. A UUID will be 128 bits, in accordance
+ * to RFC 4122:
+ * https://datatracker.ietf.org/doc/html/rfc4122
+*/
+typedef struct uuid {
+  uint8_t bytes[16];
+} uuid;
+
+#define UUID_SIZE 16                                // in bytes
+#define UUID_STRING_SIZE UUID_SIZE * sizeof(char)   // string size when printing UUID
+
 #include "esp_random.h"
 #include "openthread/logging.h"
