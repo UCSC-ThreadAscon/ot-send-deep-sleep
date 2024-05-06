17dd58155333375adc79479e8f10eafe76206c98
Simeon Tran <siatran@ucsc.edu>
2024-05-06 13:30:34 -0700

    No build erros

---
+++ components/workload/client/response_callback.c
@@ -0,0 +1,33 @@
+/**
+ * All of the code in this file is based upon the both CoAP and CoAP secure
+ * source code used as a part of the OpenThread codebase. The CoAP and
+ * CoAP secure source files can be found at:
+ * https://github.com/UCSC-ThreadAscon/openthread/tree/main/src/cli
+*/
+#include "workload.h"
+
+void handleResponse(void *aContext,
+                    otMessage *aMessage,
+                    const otMessageInfo *aMessageInfo,
+                    otError aResult)
+{
+  if (aResult != OT_ERROR_NONE) {
+    otLogWarnPlat("Response error: %s", otThreadErrorToString(aResult));
+  } else {
+    uint16_t payloadLen = getPayloadLength(aMessage);
+    char payload[payloadLen];
+    EmptyMemory(payload, payloadLen);
+    getPayload(aMessage, payload);
+
+    char senderAddr[OT_IP6_ADDRESS_STRING_SIZE];
+    otIp6AddressToString(&(aMessageInfo->mPeerAddr), senderAddr,
+                         OT_IP6_ADDRESS_STRING_SIZE);
+
+    otLogNotePlat("Response from %s of size %" PRIu16 " bytes.",
+                  senderAddr, payloadLen);
+  }
+
+  otLogNotePlat("Going to deep sleep.");
+  esp_deep_sleep_start();
+  return;
+}
