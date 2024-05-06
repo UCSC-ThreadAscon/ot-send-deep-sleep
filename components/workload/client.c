df1965ca73564ebccd99960d9e676dc1fd894839
Simeon Tran <siatran@ucsc.edu>
2024-05-06 12:37:55 -0700

    Improved comment citation

--- components/workload/client.c
+++ components/workload/client.c
@@ -1,7 +1,7 @@
 /**
  * All of the code in this file is based upon the both CoAP and CoAP secure
- * source code used as a part of the OpenThread codebase.
- *
+ * source code used as a part of the OpenThread codebase. The CoAP and
+ * CoAP secure source files can be found at:
  * https://github.com/UCSC-ThreadAscon/openthread/tree/main/src/cli
 */
 #include "workload.h"
