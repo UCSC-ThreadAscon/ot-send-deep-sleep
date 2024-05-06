403788c2c1135d16398593553ed0a18dd3dc66a4
Simeon Tran <siatran@ucsc.edu>
2024-05-06 15:44:35 -0700

    Move deep sleep wakeup function to a separate file.

--- main/handlers.c
+++ main/handlers.c
@@ -21,23 +21,6 @@ void onPowerOn(struct timeval *events, uuid *deviceId)
   return;
 }
 
-void onDeepSleepWakeup(struct timeval *events, uuid *deviceId)
-{
-  nvs_handle_t handle;
-  openReadWrite(NVS_NAMESPACE, &handle);
-
-  nvsReadArray(&handle, NVS_EVENTS_ARRAY, events, EVENTS_ARRAY_SIZE);
-  nvsReadArray(&handle, NVS_UUID, deviceId, UUID_SIZE_BYTES);
-
-#if NVS_DEBUG
-  printEventsArray(events, NUM_EVENTS);
-  printUUID(deviceId);
-#endif
-
-  nvs_close(handle);
-  return;
-}
-
 void sendEventPacket(otSockAddr *socket, uuid deviceId)
 {
   *socket = createSocket(CONFIG_SERVER_IP_ADDRESS);
