97c1acbfe89956daa8843eeacbde1aa596eab25e
Simeon Tran <siatran@ucsc.edu>
2024-05-06 12:36:49 -0700

    Specified my to-do

--- main/main.c
+++ main/main.c
@@ -2,6 +2,12 @@
 
 #define BATTERY_WAIT_TIME_MS_TEST 5000
 
+/**
+ * ---- CURRENT TO-DO ----
+ * Send one event packet every 5 seconds. Along the way, refactor your CoAP
+ * client code.
+*/
+
 void app_main(void)
 {
   initAppMain();
