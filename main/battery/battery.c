f230f4c5f92a104e9d3e8b8ce52a5d160e934da1
Simeon Tran <siatran@ucsc.edu>
2024-05-07 15:58:28 -0700

    Forgot to change

--- main/battery/battery.c
+++ main/battery/battery.c
@@ -3,7 +3,7 @@
 void moveToNextBatteryWakeup(nvs_handle_t handle, struct timeval *batteryWakeup,
                              struct timeval tvNow)
 {
-  batteryWakeup->tv_sec = getCurrentTimeval().tv_sec + BATTERY_WAIT_TIME_SEC;
+  batteryWakeup->tv_sec = tvNow.tv_sec + BATTERY_WAIT_TIME_SEC;
   nvsWriteBlob(handle, NVS_BATTERY_WAKEUP, batteryWakeup, sizeof(struct timeval));
   return;
 }
