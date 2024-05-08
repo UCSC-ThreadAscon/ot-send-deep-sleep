8caeca00b17755ce03339c1b94bb912ae1d83689
Simeon Tran <siatran@ucsc.edu>
2024-05-07 20:07:07 -0700

    Turned off flags

--- main/include/flags.h
+++ main/include/flags.h
@@ -1,6 +1,6 @@
 #pragma once
 
 /** ---- Toggles debugging print statements ---- */
-#define NVS_DEBUG 1
+#define NVS_DEBUG 0
 #define SHOW_DEBUG_STATS 1
-#define COMPARISON_DEBUG 1
+#define COMPARISON_DEBUG 0
