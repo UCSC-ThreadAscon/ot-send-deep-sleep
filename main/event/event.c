e47bb2da77700a0a1b30d40ac349df48d8a44f44
Simeon Tran <siatran@ucsc.edu>
2024-05-09 15:00:08 -0700

    Wrote down my plan for Step 1.

--- main/event/event.c
+++ main/event/event.c
@@ -1,4 +1,19 @@
 #include "main.h"
+#include "math.h"
+
+/**
+ * STEP 1: Segmentation
+ * --------------------
+ * Your experiments will run for 183 minutes, and you will need to send
+ * "n" event packets, depending on the role the device is playing. Thus,
+ * "n" is a value in {1, 10, 36}.
+ *
+ * Divide 183 minutes into "floor(183/n)" partitions. Then, for each partition,
+ * find a RANDOM MINUTE (no seconds, or ms, or us) to have an event in.
+ *
+ * This method of segmentation will AVOID DUPLICATE events, when there are two
+ * events that will be sent at the same time (or are really close).
+*/
 
 void initEventsArray(struct timeval *events,
                      struct timeval start,
