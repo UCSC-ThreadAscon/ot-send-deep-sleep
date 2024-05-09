42c30be473d4e2cb90bd20cc2223671cf7f4b974
Simeon Tran <siatran@ucsc.edu>
2024-05-09 15:41:59 -0700

    The minutes are too big.

--- components/utilities/include/time_api.h
+++ components/utilities/include/time_api.h
@@ -21,6 +21,7 @@
 #define US_TO_MINUTES(us) MS_TO_MINUTES(US_TO_MS(us))
 
 #define SECONDS_TO_MS(secs) secs * 1000
+#define MINUTES_TO_SECONDS(mins) mins * 60
 
 uint64_t toMicro(struct timeval time);
 struct timeval getTimevalNow();
--- main/event/event.c
+++ main/event/event.c
@@ -15,14 +15,14 @@
  * where R = r (mod (j - i)) + i,
  * and   r = esp_random();
  */
-struct timeval randomSeconds(struct timeval start, struct timeval end)
+struct timeval randomSeconds(struct timeval tvI, struct timeval tvJ)
 {
   struct timeval randomTime;
   randomTime.tv_usec = 0;
 
   time_t R = esp_random();
-  time_t i = start.tv_sec;
-  time_t j = end.tv_sec;
+  time_t i = tvI.tv_sec;
+  time_t j = tvJ.tv_sec;
 
   randomTime.tv_sec = (R % (j - i)) + i;
   return randomTime;
@@ -43,7 +43,7 @@ struct timeval randomSeconds(struct timeval start, struct timeval end)
  *
  *
  * |-----------|-----------|.....|-----------|
- * 0    ^     i_1    ^    i_2   i_n-1   ^    183
+ * 0    ^     a_1    ^    a_2   a_n-1   ^    183
  *      |            |                  |
  *      |            |                  |
  * one random event -|           one random event
@@ -52,7 +52,21 @@ void segmentation(struct timeval *events,
                   struct timeval start,
                   struct timeval end)
 {
-  // int numSegments = (int) floor(EXP_TIME_SECONDS / NUM_EVENTS);
+  time_t segmentDurMins = (time_t) floor(EXP_TIME_SECONDS / NUM_EVENTS);
+  time_t segmentDurSecs = MINUTES_TO_SECONDS(segmentDurMins);
+
+  struct timeval i; i.tv_usec = 0;
+  struct timeval j; j.tv_usec = 0;
+
+  i.tv_sec = start.tv_sec;
+  for (int e = 0; e < NUM_EVENTS; e++) {
+    j.tv_sec = i.tv_sec + segmentDurSecs;
+    events[e] = randomSeconds(i, j);
+
+    // Equivalent to "i.tv_sec += segmentDurSecs".
+    i.tv_sec = j.tv_sec;
+  }
+
   return;
 }
 
@@ -60,11 +74,13 @@ void initEventsArray(struct timeval *events,
                      struct timeval start,
                      struct timeval end)
 {
-  for (int i = 0; i < NUM_EVENTS; i++)
-  {
-    events[i] = randomTime(start, end);
-  }
+  segmentation(events, start, end);
 
-  qsort(events, NUM_EVENTS, sizeof(struct timeval), compareTimevals);
+  // for (int i = 0; i < NUM_EVENTS; i++)
+  // {
+  //   events[i] = randomTime(start, end);
+  // }
+
+  // qsort(events, NUM_EVENTS, sizeof(struct timeval), compareTimevals);
   return;
 }
