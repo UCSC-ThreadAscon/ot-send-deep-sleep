8a9645a8340907c46a2eb92d1c3bf61fca4580c2
Simeon Tran <siatran@ucsc.edu>
2024-05-09 14:54:30 -0700

    Forgot to delete

--- main/event/event.c
+++ main/event/event.c
@@ -12,26 +12,3 @@ void initEventsArray(struct timeval *events,
   qsort(events, NUM_EVENTS, sizeof(struct timeval), compareTimevals);
   return;
 }
-
-void printEventsIndex(uint8_t index)
-{
-  otLogNotePlat("Events index is at %" PRIu8 ".", index);
-  return;
-}
-
-void printEventsArray(struct timeval *events, int numEvents) {
-  for (int i = 0; i < numEvents; i++)
-  {
-    printEventMinutes(i + 1, events[i]);
-  }
-  return;
-}
-
-void printEventMinutes(int eventNum, struct timeval time)
-{
-  uint64_t timeMicro = toMicro(time);
-  double timeMinutes = US_TO_MINUTES((double) timeMicro);
-  otLogNotePlat("The %dth event will be in %.3f minutes.",
-                eventNum, timeMinutes);
-  return;
-}
